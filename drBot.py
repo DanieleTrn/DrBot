5fc4c480dd6ee6a438da1e0a3de4465d118de48f
DanieleTrn <danieletrn95@gmail.com>
2021-01-05 22:12:05 +0100

    050121 22:12

--- drBot.py
+++ drBot.py
@@ -15,6 +15,12 @@ lenRes = 0
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
 
+    #keyboard = InlineKeyboardMarkup(inline_keyboard= [
+               #[InlineKeyboardButton(text="Cliccami",callback_data="press")],
+                #])
+    
+    keyboardDevices = getKeyboard(getDevices(db))
+
     if content_type == "text":
         global step
         global stepPrec
@@ -24,14 +30,20 @@ def handle(msg):
         global lenRes
 
         if msg["text"] == "prova":
-            bot.sendMessage(chat_id,"Seleziona il dispositivo")
+            print(keyboardDevices)
+            bot.sendMessage(chat_id,"Seleziona il dispositivo", reply_markup=keyboardDevices)
+
+def on_callback_query(msg):
+    query_id, from_id, query_data = telepot.glance(msg, flavor="callback_query")
+
+    bot.answerCallbackQuery(query_id,text="Yeah")
 
 db = mysql.connector.connect(host="localhost", user="root", password="", database="drbot")
 
 TOKEN = '1414329712:AAHUIof6BrcAWIBz7hgZqz_-tmXBb_lyPwI'
 bot = telepot.Bot(TOKEN)
 
-bot.message_loop(handle)
+bot.message_loop({"chat": handle, "callback_query": on_callback_query})
 print("Listening...")
 
 while 1:
